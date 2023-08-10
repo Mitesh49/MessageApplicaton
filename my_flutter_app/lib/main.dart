51c2c5f70c97b72cb1724047c1fc3e8a91112f5d
Mitesh49 <m49cutz@gmail.com>
2023-08-10 22:19:52 +0530

    saving the project first try

---
+++ my_flutter_app/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Material(
+      child: Column(
+        children: [
+          Text('Hello , Im Mitesh'),
+        ],
+      ),
+    ),
+  ));
+}
+//this is the hello world sample
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
